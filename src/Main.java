94249a8289022e4a6f9d002314c639072e436e6a
Ben "Burrito" Bariteau <firba1@gmail.com>
2013-03-10 18:02:30 -0400

    Basic driver working doing stuff.

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String grammar = args[0];
+        String input = args[1];
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(new File(grammar));
+        } catch (FileNotFoundException e) {
+            System.err.println("Grammar file not found");
+            System.exit(-1);
+        }
+
+        if(scanner.hasNext()) {
+            System.out.println(scanner.next());
+        }
+    }
+}
