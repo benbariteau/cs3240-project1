b778cedbd6c01f2d7f7146749c7fad9ca8f2e439
Nathan Fan <nfan6@gatech.edu>
2013-03-11 15:07:44 -0400

    Updated Test File

--- src/Test.java
+++ src/Test.java
@@ -3,6 +3,8 @@ import java.io.File;
 public class Test {
 
 	public static void main(String[] args) {
+		System.out.println("Running with files: "+new File("test/SampleSpec").getAbsolutePath()+","+new File("test/SampleSpec").getAbsolutePath());
+		
 		Main.main(new String[]{"test/SampleSpec","test/SampleInput"});
 	}
 
