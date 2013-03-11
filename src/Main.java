f50b345998b768b326c3b288738019849ccad832
Matthew Compton <ambergleam@gmail.com>
2013-03-11 14:28:36 -0400

    Improved readability

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,24 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-//AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
 public class Main {
+	
+	/*
+	 * Main method and driver of the program
+	 * @arg[0]	Grammar
+	 * @arg[1]	Input
+	 */
     public static void main(String[] args) {
-        if (args.length < 2) {
-            System.out.println("Usage: java Main path/to/grammar path/to/input");
+    	// Must have two argument inputs - grammar and sample input
+        if (args.length != 2) {
+            System.out.println("Invalid parameters. Try:\njava Main <path/to/grammar> <path/to/input>");
             System.exit(-1);
         }
 
         String grammar = args[0];
         String input = args[1];
 
+        // Input the grammar file
         Scanner scanner = null;
         try {
             scanner = new Scanner(new File(grammar));
@@ -25,12 +32,15 @@ public class Main {
             System.exit(-1);
         }
 
+        // HashMap of classes and their class type
         Map<String, String> characterClasses = new HashMap<String, String>();
 
+        // Parse for character classes in grammar file
         Pattern pattern = Pattern.compile("\\$[A-Z-]+");
         while(scanner.hasNextLine()) {
             String line = scanner.nextLine();
             if (line.trim().equals("")) {
+            	// A blank line means switch to the token parsing
                 break;
             }
             Matcher matcher = pattern.matcher(line);
@@ -40,14 +50,17 @@ public class Main {
             characterClasses.put(charClass, rest);
         }
 
+        // Output all the character classes that we just scanned
         System.out.println("Character Classes");
         for(String key : characterClasses.keySet()) {
             System.out.println("{" + key + ", " + characterClasses.get(key) +"}");
         }
         System.out.println();
 
+        // HashMap of tokens and their token type
         Map<String, String> tokens = new HashMap<String, String>();
 
+        // Parse for character tokens
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
             Matcher matcher = pattern.matcher(line);
@@ -57,15 +70,20 @@ public class Main {
             tokens.put(token, rest);
         }
 
+        // Output all the character tokens that we just scanned
         System.out.println("Tokens");
         for(String key : tokens.keySet()) {
             System.out.println("{" + key + ", " + tokens.get(key) +"}");
         }
     }
 
+    /*
+     * Removes all whitespace from a string
+     */
     private static String removeInitialWhitespace(String substring) {
         Pattern initialWhitespace = Pattern.compile("^\\s+");
         Matcher whitespaceMatcher = initialWhitespace.matcher(substring);
         return whitespaceMatcher.replaceAll("");
     }
+    
 }
