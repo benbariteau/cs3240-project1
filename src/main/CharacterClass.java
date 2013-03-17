117152950e8fadccb832cdf255f6850ef794b81f
Nathan Fan <nfan6@gatech.edu>
2013-03-17 18:10:47 -0400

    Class for matching character classes

---
+++ src/main/CharacterClass.java
@@ -0,0 +1,16 @@
+package main;
+
+import java.util.regex.Matcher;
+
+public class CharacterClass {
+
+	private Matcher matcher;
+	private Matcher inMatcher;
+	public CharacterClass()
+	{
+	
+	}
+	
+	
+	
+}
