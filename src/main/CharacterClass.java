183833919afecf80cf450c4b6420ca843caa5e4d
Nathan Fan <nfan6@gatech.edu>
2013-03-17 18:33:05 -0400

    CharacterClass: Added stringRepresentation(), which returns this character class' regular expression.
    Added constructors and matches() method

--- src/main/CharacterClass.java
+++ src/main/CharacterClass.java
@@ -1,16 +1,41 @@
 package main;
 
-import java.util.regex.Matcher;
+/**
+ * A class that represents a character class 
+ *
+ */
+public class CharacterClass 
+{
 
-public class CharacterClass {
-
-	private Matcher matcher;
-	private Matcher inMatcher;
+	private String regex;
+	private CharacterClass superClass;
+	
 	public CharacterClass()
 	{
+	}
+	public CharacterClass(String r, CharacterClass s)
+	{
+		regex = r;
+		superClass = s;
 	
 	}
 	
+	public String stringRepresentation()
+	{
+		if (superClass == null)
+		{
+			return regex;
+		}
+		else
+		{
+			return "(?="+regex+")"+superClass.stringRepresentation();
+		}
+	}
+	
+	public boolean matches(String other)
+	{
+		return other.matches(regex);
+	}
 	
 	
 }
