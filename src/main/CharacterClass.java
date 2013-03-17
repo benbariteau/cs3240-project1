f08418f7b43f8aadf56469ee5f2c5d1f972daf19
Nathan Fan <nfan6@gatech.edu>
2013-03-17 18:47:19 -0400

    Updated Character Class

--- src/main/CharacterClass.java
+++ src/main/CharacterClass.java
@@ -10,9 +10,30 @@ public class CharacterClass
 	private String regex;
 	private CharacterClass superClass;
 	
+	
+	/**
+	 * Default constructor
+	 */
 	public CharacterClass()
 	{
 	}
+	
+	
+	/**
+	 * Constructor that only accepts a regular expression
+	 * @param r the regular expression that represents this character class
+	 */
+	public CharacterClass(String r)
+	{
+		regex = r;
+		superClass = null;
+	}
+	
+	/**
+	 * Constructor that accepts a regular expression and 
+	 * @param r the regular expression that represents this character class
+	 * @param s the CharacterClass that represents this character class' superclass
+	 */
 	public CharacterClass(String r, CharacterClass s)
 	{
 		regex = r;
@@ -20,7 +41,11 @@ public class CharacterClass
 	
 	}
 	
-	public String stringRepresentation()
+	/**
+	 * Returns the regular expression representing this character class.
+	 * @return regex the regular expression representing this character class.
+	 */
+	public String toString()
 	{
 		if (superClass == null)
 		{
@@ -28,10 +53,15 @@ public class CharacterClass
 		}
 		else
 		{
-			return "(?="+regex+")"+superClass.stringRepresentation();
+			return "(?="+regex+")"+superClass.toString();
 		}
 	}
 	
+	/**
+	 * Returns whether the parameter String matches this character class. 
+	 * @param other The parameter String to be matched to this character class
+	 * @return matches A boolean that represents whether other fits in this character Class.
+	 */
 	public boolean matches(String other)
 	{
 		return other.matches(regex);
