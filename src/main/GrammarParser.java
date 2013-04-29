6b55970bd0658323f6eac0891bb58af6e27a5f30
Ben "Burrito" Bariteau <firba1@gmail.com>
2013-04-28 21:27:41 -0400

    Fixed some stuff.

--- src/main/GrammarParser.java
+++ src/main/GrammarParser.java
@@ -82,8 +82,31 @@ public class GrammarParser {
                     s = s.substring(1).trim();
                     break;
                 case 'A':
+                case 'B':
+                case 'C':
+                case 'D':
+                case 'E':
+                case 'F':
+                case 'G':
+                case 'H':
                 case 'I':
+                case 'J':
+                case 'K':
+                case 'L':
+                case 'M':
+                case 'N':
+                case 'O':
+                case 'P':
+                case 'Q':
                 case 'R':
+                case 'S':
+                case 'T':
+                case 'U':
+                case 'V':
+                case 'W':
+                case 'X':
+                case 'Y':
+                case 'Z':
                     String ruleName = "";
                     for (i = 0; i < s.length() && token_id_chars.contains(s.charAt(i)); i++) {
                         ruleName += s.charAt(i);
